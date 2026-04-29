fd77e3ec9932f0d06426a885faff1d516ff7b39a
Raju gautam <rajukumar430435@gmail.com>
2026-04-29 19:50:18 +0530

    fixed me auth route

--- medai/backend/routes/auth.js
+++ medai/backend/routes/auth.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const authController = require("../controllers/authController");
+const { protect } = require("../middleware/auth");
 
 router.post("/register", authController.register);
 router.post("/login", authController.login);
-router.get("/me", authController.getMe);
+router.get("/me", protect, authController.getMe);
 
 module.exports = router;
